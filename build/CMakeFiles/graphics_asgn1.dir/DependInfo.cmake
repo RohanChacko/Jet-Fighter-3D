f100af70fc6396086a429004bfc6d6989244f9db
rohanchacko007 <rohanchacko007@gitlab.com>
2019-02-04 18:36:38 +0530

    Added plane, sky and water body

---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/src/airplane.cpp" "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/build/CMakeFiles/graphics_asgn1.dir/src/airplane.cpp.o"
+  "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/src/color.cpp" "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/src/input.cpp" "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/src/main.cpp" "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/src/nonedit.cpp" "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/src/other_handlers.cpp" "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/src/timer.cpp" "/home/rohan/Graphics/Assignments/Assgn_2/jet-fighter-3d/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/graphics_asgn2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphics_asgn2.dir/src/main.cpp.o"
+  "CMakeFiles/graphics_asgn2.dir/src/other_handlers.cpp.o"
+  "CMakeFiles/graphics_asgn2.dir/src/airplane.cpp.o"
+  "CMakeFiles/graphics_asgn2.dir/src/timer.cpp.o"
+  "CMakeFiles/graphics_asgn2.dir/src/color.cpp.o"
+  "CMakeFiles/graphics_asgn2.dir/src/floor.cpp.o"
+  "CMakeFiles/graphics_asgn2.dir/src/nonedit.cpp.o"
+  "CMakeFiles/graphics_asgn2.dir/src/input.cpp.o"
+  "graphics_asgn2.pdb"
+  "graphics_asgn2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphics_asgn2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
